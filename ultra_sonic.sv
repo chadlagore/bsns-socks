f5143527fa9da282c9037c3fc638c68f4fad887d
chadlagore <chad.d.lagore@gmail.com>
2017-02-03 14:04:20 -0800

    read and write signals added

--- ultra_sonic.sv
+++ ultra_sonic.sv
@@ -11,14 +11,16 @@ module ultra_sonic(
     echo_high, // Recieving data from sensor.
 
     // OUTPUTS //
-    count_out, // Number of clock cycles trigger held high.
     pulse_out, // Output pulse to initialize state machine.
-    count_ready_out, // Distance is ready
 
     // MEMORY_MAP ////
-    addr
-
+    addr,
+    read_en,
+    write_en,
+    write_data,
+    read_data // Read out.
   );
+
   parameter COUNT_WIDTH = 32;
   parameter DELAY_WIDTH = 8;
 
@@ -28,18 +30,17 @@ module ultra_sonic(
   input logic echo_high;
 
   //// OUTPUTS ////
-  output logic [COUNT_WIDTH-1:0] count_out;
-
-  output logic count_ready_out;
-
   output logic pulse_out;
 
   // MEMORY_MAP ////
   input logic addr;
+  input logic read_en;
+  input logic write_en;
+  input logic [31:0] write_data;
+  input logic [31:0] read_data;
 
   // State
   logic [9:0] state;
-  // logic [9:0] nextState;
                                 //  9876_543210
   parameter idle              = 10'b0000_000000;
   parameter init_pulse        = 10'b0001_100011;
@@ -48,16 +49,15 @@ module ultra_sonic(
   parameter head_to_echo      = 10'b0101_000101;
 
   // Logic.
-  logic trigger_in;
-  logic [DELAY_WIDTH-1:0] delay_pulse_count;
-  logic counting_echo;
   logic waiting_for_echo;
   logic reset_echo_count;
+  logic count_ready_out;
+  logic [COUNT_WIDTH-1:0] count_out;
+  logic [DELAY_WIDTH-1:0] delay_pulse_count;
 
   // Outputs.
   assign pulse_out = state[1];
   assign reset_echo_count = state[2];
-  assign counting_echo = state[3];
   assign waiting_for_echo = state[4];
   assign count_ready_out = ~echo_high; // cout available when echo low.
 
@@ -109,6 +109,14 @@ module ultra_sonic(
     else if (reset_echo_count) count_out <= 0;
     else if (echo_high) count_out <= count_out + 1'b1;
     else count_out <= count_out;
+  end
+
+  always_comb
+  begin
+    if(addr)
+      read_data <= count_out;
+    else
+      read_data <= {31'b0, count_ready_out};
   end
 
 endmodule
