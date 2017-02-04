01e8e2e125888fc045463223aba7fa26c7ca2da7
chadlagore <chad.d.lagore@gmail.com>
2017-02-03 17:14:39 -0800

    merging wifi module into this branch

--- ultra_sonic.sv
+++ ultra_sonic.sv
@@ -8,17 +8,13 @@ module ultra_sonic(
     // INPUTS //
     clk, // 50MHz
     reset_all,
-    echo_high, // Recieving data from sensor.
-
-    // OUTPUTS //
-    pulse_out, // Output pulse to initialize state machine.
 
     // MEMORY_MAP ////
     addr,
-    read_en,
-    write_en,
-    write_data,
-    read_data // Read out.
+    read_data, // Read out.
+
+    echo,
+    trigger
   );
 
   parameter COUNT_WIDTH = 32;
@@ -27,16 +23,8 @@ module ultra_sonic(
   //// INPUTS ////
   input logic clk, reset_all;
 
-  input logic echo_high;
-
-  //// OUTPUTS ////
-  output logic pulse_out;
-
   // MEMORY_MAP ////
   input logic addr;
-  input logic read_en;
-  input logic write_en;
-  input logic [31:0] write_data;
   output logic [31:0] read_data;
 
   // State
@@ -55,11 +43,15 @@ module ultra_sonic(
   logic [COUNT_WIDTH-1:0] count_out;
   logic [DELAY_WIDTH-1:0] delay_pulse_count;
 
+  // Signal from/to sensor.
+  input logic echo;
+  output logic pulse;
+
   // Outputs.
-  assign pulse_out = state[1];
+  assign pulse = state[1];
   assign reset_echo_count = state[2];
   assign waiting_for_echo = state[4];
-  assign count_ready_out = ~echo_high; // cout available when echo low.
+  assign count_ready_out = ~echo; // cout available when echo low.
 
   // State logic.
   always_ff @(posedge clk or negedge reset_all)
@@ -79,7 +71,7 @@ module ultra_sonic(
                      end
         /////// WAIT_FOR_ECHO STATE ////////
         wait_for_echo:  begin
-                          if (echo_high == 1'b1) state <= head_to_echo;
+                          if (echo == 1'b1) state <= head_to_echo;
                           else state <= wait_for_echo;
                         end
         /////// WAIT_FOR_ECHO STATE ////////
@@ -88,7 +80,7 @@ module ultra_sonic(
                         end
         /////// ON_ECHO STATE ////////
         on_echo:  begin
-                      if (echo_high == 1'b0) state <= init_pulse;
+                      if (echo == 1'b0) state <= init_pulse;
                       else state <= on_echo;
                   end
         endcase
@@ -98,7 +90,7 @@ module ultra_sonic(
   always_ff @(posedge clk or negedge reset_all)
   begin
     if (~reset_all) delay_pulse_count <= {{DELAY_WIDTH{0}}, 1'b1};
-    else if (pulse_out) delay_pulse_count <= delay_pulse_count + 1'b1;
+    else if (pulse) delay_pulse_count <= delay_pulse_count + 1'b1;
     else count_out <= count_out;
   end
 
@@ -107,7 +99,7 @@ module ultra_sonic(
   begin
     if (~reset_all) count_out <= {COUNT_WIDTH{0}};
     else if (reset_echo_count) count_out <= 0;
-    else if (echo_high) count_out <= count_out + 1'b1;
+    else if (echo) count_out <= count_out + 1'b1;
     else count_out <= count_out;
   end
 
--- ultrasonic_tb.sv
+++ ultrasonic_tb.sv
@@ -1,68 +1,56 @@
 `default_nettype none
 module ultra_sonic_tb();
   // Inputs from nios.
-  logic clk, reset, start, addr;
-
-  // Inputs from sensor.
-  logic echo_high;
+  logic clk, reset, addr;
 
   // Outputs to nios
-  logic count_ready, active;
-  logic [31:0] read, write;
-
-  // Outputs to sensor
-  logic pulse_out;
+  logic [31:0] read;
+  logic echo, pulse;
 
   ultra_sonic DUT(
       // inputs
       .clk(clk),
       .reset_all(~reset),
 
-      // input from sensor.
-      .echo_high(echo_high),
-      .pulse_out(pulse_out),
-
       // outputs
       .addr(addr),
-      .read_en(1'b1),
-      .write_en(1'b1),
-      .write_data(write),
-      .read_data(read)
+      .read_data(read),
+      .echo(echo),
+      .pulse(pulse)
     );
 
-  always begin
-    addr = 1;
+  initial begin
+    addr = 0;
     reset = 1;
     #1 clk = 0;
     #1 clk = 1;
     reset = 0;
     #1 clk = 0;
     #1 clk = 1;
-    #1 start = 1;
 
     for (int i = 0; i < 750; i++) begin
       #1 clk = ~clk;
     end
 
-    echo_high = 1;
+    echo = 1;
 
     for (int i = 0; i < 50; i++) begin
       #1 clk = ~clk;
     end
 
-    echo_high = 0;
+    echo = 0;
 
     for (int i = 0; i < 750; i++) begin
       #1 clk = ~clk;
     end
 
-    echo_high = 1;
+    echo = 1;
 
     for (int i = 0; i < 750; i++) begin
       #1 clk = ~clk;
     end
 
-    echo_high = 0;
+    echo = 0;
 
     for (int i = 0; i < 750; i++) begin
       #1 clk = ~clk;
