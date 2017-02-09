4f82991771b87345c97b834a5b7d58bfb422ab28
chadlagore <chad.d.lagore@gmail.com>
2017-02-08 18:21:54 -0800

    simulates well

--- ultra_sonic.sv
+++ ultra_sonic.sv
@@ -1,113 +1,138 @@
-`define idle        0
-`define trigger     1
-`define wait_burst  2
-`define echo_pulse  3
+/// IMPORTANT ////
+/// Formula to get the length of the echo from count data:
+  /// t_pulse = 2 * count.
 
 `default_nettype none
 module ultra_sonic(
     // INPUTS //
     clk, // 50MHz
-    reset_all,
+    reset_l,
 
-    // MEMORY_MAP ////
-    addr,
-    read_data, // Read out.
+    // DATA OUT ////
+    read_data,
+    read_data_valid,
 
+    // INPUT FROM GPIO //
     echo,
+
+    // OUPUT TO GPIO //
     trigger
   );
 
+  parameter STATE_BITS = 7;
   parameter COUNT_WIDTH = 32;
-  parameter DELAY_WIDTH = 8;
+  // Delay for 20us: 10bits = lg(1025 cycles).
+  parameter DELAY_WIDTH = 10;
+  // Delay for 60ms: 22bits = lg(2000000 cycles).
+  parameter STALL_WIDTH = 10;
 
   //// INPUTS ////
-  input logic clk, reset_all;
+  input logic clk, reset_l;
 
   // MEMORY_MAP ////
-  input logic addr;
+  output logic read_data_valid;
   output logic [31:0] read_data;
 
+  // GPIO IN/OUT //
+  input logic echo;
+  output logic trigger;
+
+  logic [3:0] state_bits;
+
   // State
-  logic [9:0] state;
-                                //  9876_543210
-  parameter idle              = 10'b0000_000000;
-  parameter init_pulse        = 10'b0001_100011;
-  parameter wait_for_echo     = 10'b0011_110001;
-  parameter on_echo           = 10'b0100_101001;
-  parameter head_to_echo      = 10'b0101_000101;
+  logic [STATE_BITS-1:0] state;
+                                // 5432_10
+  parameter idle              = 6'b0000_00;
+  parameter init_trigger      = 6'b0001_01;
+  parameter wait_for_echo     = 6'b0011_00;
+  parameter on_echo           = 6'b0100_00;
+  parameter head_to_echo      = 6'b0101_00;
+  parameter stall             = 6'b0110_10;
 
   // Logic.
-  logic waiting_for_echo;
-  logic reset_echo_count;
-  logic count_ready_out;
   logic [COUNT_WIDTH-1:0] count_out;
-  logic [DELAY_WIDTH-1:0] delay_pulse_count;
-
-  // Signal from/to sensor.
-  input logic echo;
-  output logic pulse;
+  logic [DELAY_WIDTH-1:0] delay_trigger_count;
+  logic [STALL_WIDTH-1:0] stall_count;
 
   // Outputs.
-  assign pulse = state[1];
-  assign reset_echo_count = state[2];
-  assign waiting_for_echo = state[4];
-  assign count_ready_out = ~echo; // cout available when echo low.
+  assign trigger = state[0];
+  assign read_data_valid = state[1];
+  assign state_bits = state[5:2];
 
   // State logic.
-  always_ff @(posedge clk or negedge reset_all)
+  always_ff @(posedge clk or negedge reset_l)
   begin
-    if(~reset_all)
+    if(~reset_l)
       state <= idle;
     else
       case(state)
-        /////// IDLE STATE ////////
         idle: begin
-                state <= init_pulse;
+                state <= init_trigger;
               end
-        /////// INIT_PULSE STATE ////////
-        init_pulse:  begin
-                        if (delay_pulse_count == 9'b0) state <= wait_for_echo;
-                        else state <= init_pulse;
+        // Count trigger high for 20us.
+        init_trigger:  begin
+                      if (delay_trigger_count == 0) state <= wait_for_echo;
+                      else state <= init_trigger;
                      end
-        /////// WAIT_FOR_ECHO STATE ////////
+        // Wait for echo to go high.
         wait_for_echo:  begin
                           if (echo == 1'b1) state <= head_to_echo;
                           else state <= wait_for_echo;
                         end
-        /////// WAIT_FOR_ECHO STATE ////////
-        head_to_echo:  begin
-                          state <= on_echo;
-                        end
-        /////// ON_ECHO STATE ////////
+        // Echo high start counting.
+        head_to_echo: state <= on_echo;
+
+        // Waiting for echo to go low.
         on_echo:  begin
-                      if (echo == 1'b0) state <= init_pulse;
+                      if (echo == 1'b0) state <= stall;
                       else state <= on_echo;
+                  end
+        // Waiting for 60ms to be safe.
+        stall:  begin
+                      if (stall_count == 1'b0) state <= init_trigger;
+                      else state <= stall;
                   end
         endcase
   end
 
   // Wait counter
-  always_ff @(posedge clk or negedge reset_all)
+  always_ff @(posedge clk or negedge reset_l)
   begin
-    if (~reset_all) delay_pulse_count <= {{DELAY_WIDTH{0}}, 1'b1};
-    else if (pulse) delay_pulse_count <= delay_pulse_count + 1'b1;
+    if (~reset_l)
+      delay_trigger_count <= {{DELAY_WIDTH{0}}, 1'b1};
+    else if (state == stall)
+      delay_trigger_count <= {{(DELAY_WIDTH-1){0}}, 1'b1};
+    else if (state == init_trigger)
+      delay_trigger_count <= delay_trigger_count + 1'b1;
+    else
+      delay_trigger_count <= delay_trigger_count;
+  end
+
+  // Echo counter!
+  always_ff @(posedge clk or negedge reset_l)
+  begin
+    if (~reset_l) count_out <= {COUNT_WIDTH{0}};
+    else if (state == init_trigger) count_out <= {{COUNT_WIDTH{0}}, 1'b1};
+    else if (echo) count_out <= count_out + {{(COUNT_WIDTH-1){0}}, 1'b1};
     else count_out <= count_out;
   end
 
-  // Trigger counter.
-  always_ff @(posedge clk or negedge reset_all)
+  // Stall counter.
+  always_ff @(posedge clk or negedge reset_l)
   begin
-    if (~reset_all) count_out <= {COUNT_WIDTH{0}};
-    else if (reset_echo_count) count_out <= 0;
-    else if (echo) count_out <= count_out + 1'b1;
-    else count_out <= count_out;
+    if (~reset_l) stall_count <= {STALL_WIDTH{0}};
+    else if (state == init_trigger) stall_count <= {{STALL_WIDTH{0}}, 1'b1};
+    else if (state == stall)
+      stall_count <= stall_count + {{(STALL_WIDTH-1){0}}, 1'b1};
+    else stall_count <= stall_count;
   end
 
-  always_ff @(posedge clk or negedge reset_all)
+  // Data out register.
+  always_ff @(posedge clk or negedge reset_l)
   begin
-    if(~reset_all) read_data <= 32'b0;
-    else if(addr) read_data <= count_out;
-    else read_data <= {31'b0, count_ready_out};
+    if(~reset_l) read_data <= 32'b0;
+    else if(read_data_valid) read_data <= count_out;
+    else read_data <= read_data;
   end
 
 endmodule
--- ultrasonic_tb.sv
+++ ultrasonic_tb.sv
@@ -5,56 +5,54 @@ module ultra_sonic_tb();
 
   // Outputs to nios
   logic [31:0] read;
-  logic echo, pulse;
+  logic read_data_valid;
+
+  logic echo, trigger;
+  logic on_stall;
 
   ultra_sonic DUT(
       // inputs
       .clk(clk),
-      .reset_all(~reset),
+      .reset_l(~reset),
 
       // outputs
-      .addr(addr),
       .read_data(read),
+      .read_data_valid(read_data_valid),
+
+      // GPIO
       .echo(echo),
-      .pulse(pulse)
+      .trigger(trigger)
     );
 
   initial begin
-    addr = 0;
     reset = 1;
     #1 clk = 0;
     #1 clk = 1;
     reset = 0;
     #1 clk = 0;
     #1 clk = 1;
+    for(int j=0; j<15; j++) begin
 
-    for (int i = 0; i < 750; i++) begin
-      #1 clk = ~clk;
+      // Should send trigger.
+      for (int i = 0; i < 3000; i++) begin
+        #1 clk = ~clk;
+      end
+
+      // Simulate echo goes high.
+      echo = 1;
+
+      // Wait for 50 cycles with echo high.
+      for (int i = 0; i < j * 300; i++) begin
+        #1 clk = ~clk;
+      end
+
+      // Echo drops, we go to stall.
+      echo = 0;
+
+      // Should stall, then head to trigger init.
+      for (int i = 0; i < 2000; i++) begin
+        #1 clk = ~clk;
+      end
     end
-
-    echo = 1;
-
-    for (int i = 0; i < 50; i++) begin
-      #1 clk = ~clk;
-    end
-
-    echo = 0;
-
-    for (int i = 0; i < 750; i++) begin
-      #1 clk = ~clk;
-    end
-
-    echo = 1;
-
-    for (int i = 0; i < 750; i++) begin
-      #1 clk = ~clk;
-    end
-
-    echo = 0;
-
-    for (int i = 0; i < 750; i++) begin
-      #1 clk = ~clk;
-    end
-
   end
 endmodule
