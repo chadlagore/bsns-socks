3ef7cbd6008cbdd1c35f2582131695ac937debde
chadlagore <chad.d.lagore@gmail.com>
2017-02-13 20:59:37 -0800

    reading from register :+1:

--- avalon_distance_module_interface.sv
+++ avalon_distance_module_interface.sv
@@ -5,81 +5,76 @@
  * fsm module which is instantiated herein. Module cannot be
  * written to --only read from. Current status and data output
  * are always maintained internally and written to data_out for. */
-module avalon_distance_module_interface
-(
-	// avalon interface inputs
-	input logic clk,
-	input logic reset_l,
-	input logic [15:0] address,
-	input logic io_select,
+module avalon_distance_module_interface (
+    	// avalon interface inputs
+    	clk, reset_l, address, io_select,
 
-	// ultrasonic distance sensor inputs
-	input logic echo,
+    	// ultrasonic distance sensor inputs
+    	echo,
 
+    	// avalon interface outputs
+    	read_data, data_out,
+
+    	// ultrasonic distance sensor outputs
+    	trigger, flash
+    );
+
+	// status and data memory locations
+	parameter STATUS = 16'h0904;
+	parameter DATA_OUT = 16'h0900;
+
+	//// INPUTS ////
+	input logic clk, reset_l, io_select;
+	input logic [15:0] address;
+
+	//// OUTPUTS ////
 	// avalon interface outputs
-	output logic [15:0] read_data,
+	output logic [15:0] read_data;
+	output logic [15:0] data_out;
 
 	// ultrasonic distance sensor outputs
-	output logic trigger,
-	output logic got_request,
-	output logic [15:0] data_out
-	
-);
+	output logic trigger;
+	input logic echo;
+	output logic flash;
 
-// status and data memory locations
-parameter STATUS = 16'h0904;
-parameter DATA_OUT = 16'h0900;
+	// internal wires
+	logic status_out;
+	logic dist_mod_status;
+	logic read_data_valid;
+	logic [31:0] dist_mod_data;
 
-// internal wires
-logic status_out;
-//logic [15:0] data_out;
-logic dist_mod_status;
-logic [31:0] dist_mod_data;
+    // instantiation of dist sensor fsm
+	ultra_sonic us(
+		.clk(clk),
+		.reset_l(reset_l),
+		.read_data(dist_mod_data),
+		.read_data_valid(dist_mod_status),
+		.echo(echo),
+		.trigger(trigger)
+	);
 
-// instantiation of dist sensor fsm
-ultra_sonic us(
-	.clk(clk),
-	.reset_l(reset_l),
-	.read_data(dist_mod_data),
-	.read_data_valid(dist_mod_status),
-	.echo(echo),
-	.trigger(trigger)
-);
+	// Respond to incoming request for data.
+    always_ff @(posedge clk or negedge reset_l) begin
+        if(~reset_l) read_data <= 16'bz;
+        else if(io_select) begin
+			if(address == STATUS)
+				read_data <= {15'b0, dist_mod_status};
+			else if(address == DATA_OUT)
+				read_data <= (dist_mod_data[15:0] >> 4);
+			end
+        else read_data <= 16'bz;
+    end
 
-always @(posedge clk, negedge reset_l) begin
-	// reset
-	if (~reset_l) status_out <= 1'b0;
-	// if status becomes high
-	else if (dist_mod_status) status_out <= 1'b1;
-	// if data is read from the address or reset
-	else if (address == DATA_OUT) status_out <= 1'b0;
-	// otherwise output status
-	else status_out <= status_out;
-end
+	 // Store dist module data in hexes.
+	always_ff @(posedge clk or negedge reset_l) begin
+		if(~reset_l) data_out <= 16'bz;
+		else if(io_select) begin
+			if(address == STATUS)
+				data_out <= {15'b0, dist_mod_status};
+			else if(address == DATA_OUT)
+				data_out <= dist_mod_data[15:0];
+			end
+		else data_out <= 16'bz;
+    end
 
-always @(posedge clk, negedge reset_l) begin
-	// asynchronous reset from push button
-	if (~reset_l) data_out = 16'b0;
-	// if distance module status is high, we have a new reading
-	else if (dist_mod_status == 1) data_out <= dist_mod_data[15:0];
-	// otherwise simply output the same signal
-	else data_out <= data_out;
-end
-
-always @(posedge clk, negedge reset_l) begin
-	// reset
-	if (~reset_l) read_data <= 16'b0;
-	// if address == 900, output data register signal
-	else if (address == DATA_OUT) read_data <= data_out;
-	// else if address == 904, output status register
-	else if (address == STATUS) read_data <= {15'b0, status_out};
-	// else output 0
-	else read_data <= 16'bZ;
-end // end output conditional statements
-
-always @(posedge clk, negedge reset_l) begin
-	if (~reset_l) got_request <= 1'b0;
-	else if (address == DATA_OUT) got_request <= 1'b1;
-	else got_request <= got_request;
-end
 endmodule
