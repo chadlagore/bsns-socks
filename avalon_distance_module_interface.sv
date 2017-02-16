2d2aa1974980c34d9ee6ec2881d7ebc52d484ea6
chadlagore <chad.d.lagore@gmail.com>
2017-02-15 16:31:58 -0800

    car counter

--- avalon_distance_module_interface.sv
+++ avalon_distance_module_interface.sv
@@ -20,6 +20,8 @@ module avalon_distance_module_interface (
     );
 
 	// status and data memory locations
+    parameter CAR_BIT = 16'h090C;
+    parameter CAR_COUNT = 16'h0908;
 	parameter STATUS = 16'h0904;
 	parameter DATA_OUT = 16'h0900;
 
@@ -43,15 +45,28 @@ module avalon_distance_module_interface (
 	logic read_data_valid;
 	logic [31:0] dist_mod_data;
 
+    logic [5:0] car_count;
+    logic car_detected;
+
     // instantiation of dist sensor fsm
 	ultra_sonic us(
-		.clk(clk),
-		.reset_l(reset_l),
-		.read_data(dist_mod_data),
-		.read_data_valid(dist_mod_status),
-		.echo(echo),
-		.trigger(trigger)
-	);
+        	.clk(clk),
+        	.reset_l(reset_l),
+        	.read_data(dist_mod_data),
+        	.read_data_valid(dist_mod_status),
+        	.echo(echo),
+        	.trigger(trigger)
+	       );
+
+    car_counter cc(
+            .clk(clk),
+            .reset_l(reset_l),
+            .read_signal(io_select),
+            .calibrate(1'b1), // auto calibrate.
+            .distance(dist_mod_data),
+            .car_count(car_count),
+            .car(car_detected)
+        );
 
 	// Respond to incoming request for data.
     always_ff @(posedge clk or negedge reset_l) begin
@@ -61,17 +76,21 @@ module avalon_distance_module_interface (
 				read_data <= {15'b0, dist_mod_status};
 			else if(address == DATA_OUT)
 				read_data <= (dist_mod_data[15:0] >> 4);
+            else if (address == CAR_BIT)
+				read_data <= {15'b0, car_detected};
+            else if (address == CAR_COUNT)
+				read_data <= car_count;
 			end
         else read_data <= 16'bz;
     end
 
-	 // Store dist module data in hexes.
+	// Store car count module data in hexes.
 	always_ff @(posedge clk or negedge reset_l) begin
 		if(~reset_l) data_out <= 16'bz;
 		else if(io_select) begin
-			if(address == STATUS)
+			if (address == STATUS)
 				data_out <= {15'b0, dist_mod_status};
-			else if(address == DATA_OUT)
+			else if (address == DATA_OUT)
 				data_out <= dist_mod_data[15:0];
 			end
 		else data_out <= 16'bz;
